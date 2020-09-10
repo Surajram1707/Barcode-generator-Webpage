2220ed1b36d634a921ee34704be705dcc483c6ec
Surajram.S <surajram1707@gmail.com>
2020-09-10 10:43:43 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function generateBarcode(){
-  let barcodeValue = document.getElementById('barcode-value').value
-  JsBarcode('#barcode',barcodeValue,{
-    background: 'pink',
-    lineColor:'purple'
-  })
-}
